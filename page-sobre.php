1988f27ba84b9f210ebdf944ef73663a1e8dbf0c
Weverton Monteiro <wevertonmonteiroc@gmail.com>
2022-03-07 13:04:05 -0300

    Inicio da utilização dos custom fields, para edição de conteúdo,na page-sobre.php

--- page-sobre.php
+++ page-sobre.php
@@ -12,11 +12,8 @@
     </div>
 
     <div class="grid-8">
-        <h2>História</h2>
-        <p>O incentivo ao avanço tecnológico, assim como a mobilidade dos capitais internacionais auxilia a preparação e
-            a composição dos níveis de motivação departamental.</p>
-        <p>Gostaria de enfatizar que o desenvolvimento contínuo de distintas formas de atuação prepara-nos para
-            enfrentar situações atípicas decorrentes do remanejamento dos quadros funcionais.</p>
+        <h2><?php the_field('titulo_historia'); ?></h2>
+        <?php the_field('texto_da_historia'); ?>
         <h2>Visão</h2>
         <p>Não obstante, a expansão dos mercados mundiais faz parte de um processo de gerenciamento de alternativas às
             soluções ortodoxas.</p>
