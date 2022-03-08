34426f98c4ff4e710efe17bd4755cd694c4b8271
Weverton Monteiro <wevertonmonteiroc@gmail.com>
2022-03-08 17:16:01 -0300

    Adição dos the_fields correspondentes a SEO: title_seo, description_seo)

--- header.php
+++ header.php
@@ -1,31 +1,31 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title><?php bloginfo('name'); ?> | <?php wp_title(''); ?>  <?php the_field('title_seo'); ?></title>
+        <meta name="description" content="<?php bloginfo('name'); ?> | <?php wp_title(''); ?> <?php the_field('description_seo'); ?>">
 
-<head>
-    <meta charset="utf-8">
-    <title><?php bloginfo('name'); ?></title>
+        <link href='https://fonts.googleapis.com/css?family=Alegreya+SC' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
+        <!-- Header Wordpress -->
+        <?php wp_head(); ?>
+        <!-- Fecha Header Wordpress -->
+    </head>
 
-    <link href='https://fonts.googleapis.com/css?family=Alegreya+SC' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
-    <!-- Header Wordpress -->
-    <?php wp_head(); ?>
-    <!-- Fecha Header Wordpress -->
-</head>
+    <body>
 
-<body>
+        <header>
+            <nav>
+                <ul>
+                    <li class="current_page_item"><a href="/">Menu</a></li>
+                    <li><a href="/testewpp/sobre/">Sobre</a></li>
+                    <li><a href="/testewpp/contato/">Contato</a></li>
+                </ul>
+            </nav>
 
-    <header>
-        <nav>
-            <ul>
-                <li class="current_page_item"><a href="/">Menu</a></li>
-                <li><a href="/testewpp/sobre/">Sobre</a></li>
-                <li><a href="/testewpp/contato/">Contato</a></li>
-            </ul>
-        </nav>
+            <h1><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/rest.png" alt="Rest"></h1>
 
-        <h1><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/rest.png" alt="Rest"></h1>
-
-        <?php $contato = get_page_by_title('contato'); ?>
-        <p><?php the_field('endereco_header', $contato); ?></p>
-        <p class="telefone"><?php the_field('telefone_header', $contato); ?></p>
-    </header>
+            <?php $contato = get_page_by_title('contato'); ?>
+            <p><?php the_field('endereco_header', $contato); ?></p>
+            <p class="telefone"><?php the_field('telefone_header', $contato); ?></p>
+        </header>
--- page-contato.php
+++ page-contato.php
@@ -19,26 +19,7 @@
         </div>
         <?php endwhile; else : endif; ?>
 
-        <div class="grid-1-3 contato-item">
-            <h2>Dados</h2>
-            <p>21 2422-9999</p>
-            <p>contato@rest.com</p>
-            <p>facebook.com/rest/</p>
-        </div>
-        <div class="grid-1-3 contato-item">
-            <h2>Horários</h2>
-            <p>Segunda à Sexta: 10 às 23</p>
-            <p>Sábado: 14 às 23</p>
-            <p>Domingo: 14 às 22</p>
-        </div>
-        <div class="grid-1-3 contato-item">
-            <h2>Endereço</h2>
-            <p>Rua Marechal, 29</p>
-            <p>Copacabana - Rio de Janeiro</p>
-            <p>Brasil - Terra - Via Láctea</p>
-        </div>
     </section>
-<?php endwhile;
-else : endif; ?>
+<?php endwhile; else : endif; ?>
 
 <?php get_footer(); ?>
